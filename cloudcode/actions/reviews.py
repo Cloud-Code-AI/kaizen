a9144e7b97f7b7536f5a719bde35d0cc90a408a7
Saurav Panda <sgp65@cornell.edu>
2024-04-06 10:57:45 -0400

    refc: moved module to Class based structure

--- cloudcode/actions/reviews.py
+++ cloudcode/actions/reviews.py
@@ -1,28 +1,26 @@
 from cloudcode.helpers import output, parser
-from cloudcode.llms.provider import chat_completion
+from cloudcode.llms.provider import LLMProvider
 from cloudcode.llms.prompts import CODE_REVIEW_PROMPT, CODE_REVIEW_SYSTEM_PROMPT
 import logging
 
-logger = logging.getLogger(__name__)
 
+class CodeReviewer:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.provider = LLMProvider(system_prompt=CODE_REVIEW_SYSTEM_PROMPT)
 
-def review_pull_request(
-    diff_text: str,
-    pull_request_title: str,
-    pull_request_desc: str,
-):
-    prompt = CODE_REVIEW_PROMPT.format(
-        PULL_REQUEST_TITLE=pull_request_title,
-        PULL_REQUEST_DESC=pull_request_desc,
-        CODE_DIFF=diff_text,
-    )
+    def review_pull_request(
+        self, diff_text: str, pull_request_title: str, pull_request_desc: str
+    ):
+        prompt = CODE_REVIEW_PROMPT.format(
+            PULL_REQUEST_TITLE=pull_request_title,
+            PULL_REQUEST_DESC=pull_request_desc,
+            CODE_DIFF=diff_text,
+        )
 
-    resp = chat_completion(
-        prompt,
-        system_prompt=CODE_REVIEW_SYSTEM_PROMPT,
-    )
+        resp = self.provider.chat_completion(prompt)
 
-    body = output.json_to_markdown(parser.extract_json(resp))
+        body = output.json_to_markdown(parser.extract_json(resp))
 
-    # Share the review on pull request
-    return body
+        # Share the review on pull request
+        return body
