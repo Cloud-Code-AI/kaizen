281bf5a5fcf620acc9fcd27b201116f6b8f76622
Shreyash Gupta <sg973@cornell.edu>
2024-04-10 04:31:20 +0530

    feat: Basic documentation

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,7 +3,9 @@ const withNextra = require('nextra')({
     themeConfig: './theme.config.jsx'
   })
    
-  module.exports = withNextra()
+  module.exports = withNextra({
+    basePath: '/docs'
+})
    
   // If you have other Next.js configurations, you can pass them as the parameter:
   // module.exports = withNextra({ /* other next.js config */ })
