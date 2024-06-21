e57682a3cd56f4862ce0e270f9749b95f95d2d36
Saurav Panda <sgp65@cornell.edu>
2024-06-21 19:46:19 -0400

    refc: removed llm retry methods for json parsing

--- kaizen/reviewer/code_review.py
+++ kaizen/reviewer/code_review.py
@@ -67,9 +67,7 @@ class CodeReviewer:
         if self.provider.is_inside_token_limit(PROMPT=prompt):
             self.logger.debug("Processing Directly from Diff")
             resp, usage = self.provider.chat_completion(prompt, user=user)
-            review_json, total_usage = parser.extract_json_with_llm_retry(
-                self.provider, resp, total_usage
-            )
+            review_json = parser.extract_json(resp)
             reviews = review_json["review"]
             total_usage = self.provider.update_usage(total_usage, usage)
         else:
@@ -95,9 +93,7 @@ class CodeReviewer:
                         continue
                     resp, usage = self.provider.chat_completion(prompt, user=user)
                     total_usage = self.provider.update_usage(total_usage, usage)
-                    review_json, total_usage = parser.extract_json_with_llm_retry(
-                        self.provider, resp, total_usage
-                    )
+                    review_json = parser.extract_json(resp)
                     reviews.extend(review_json["review"])
 
         topics = self.merge_topics(reviews=reviews)
@@ -135,10 +131,7 @@ class CodeReviewer:
             self.logger.debug("Processing Directly from Diff")
             resp, usage = self.provider.chat_completion(prompt, user=user)
             total_usage = self.provider.update_usage(total_usage, usage)
-            json_data, total_usage = parser.extract_json_with_llm_retry(
-                self.provider, resp, total_usage
-            )
-            desc = json_data["desc"]
+            desc = parser.extract_json(resp)["desc"]
         else:
             self.logger.debug("Processing Based on files")
             # We recurrsively get feedback for files and then get basic summary
@@ -160,18 +153,13 @@ class CodeReviewer:
                         continue
                     resp, usage = self.provider.chat_completion(prompt, user=user)
                     total_usage = self.provider.update_usage(total_usage, usage)
-                    desc_json, total_usage = parser.extract_json_with_llm_retry(
-                        self.provider, resp, total_usage
-                    )
+                    desc_json = parser.extract_json(resp)
                     descs.append(desc_json["desc"])
 
             prompt = MERGE_PR_DESCRIPTION_PROMPT.format(DESCS=json.dumps(descs))
             resp, usage = self.provider.chat_completion(prompt, user=user)
             total_usage = self.provider.update_usage(total_usage, usage)
-            resp, total_usage = parser.extract_json_with_llm_retry(
-                self.provider, resp, total_usage
-            )
-            desc = resp["desc"]
+            desc = parser.extract_json(resp)["desc"]
         body = output.create_pr_description(desc, pull_request_desc)
         prompt_cost, completion_cost = self.provider.get_usage_cost(
             total_usage=total_usage
